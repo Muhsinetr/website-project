c698d738a09409c15d85d236cfbb071129406688
Muhsinetr <72331917+Muhsinetr@users.noreply.github.com>
2023-10-01 20:06:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+if("serviceWorker" in navigator){
+    navigator.serviceWorker.register("sworker.js").then(registration=>{
+    console.log("Service worker registered")
+ }).catch(error=>{
+        console.log("Service worker error")
+        console.log(error)
+ })
+}else{
+    alert("Service worker not working")
+}
